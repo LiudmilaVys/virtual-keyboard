1864814ab05d23881aa0f045cf2a753940a77f0e
Liudmila Vysotskaya <liudmila.vys@gmail.com>
2022-05-03 21:12:12 +0200

    feat: highlight keys on the virtual keyboard

--- src/keyboard.js
+++ src/keyboard.js
@@ -17,6 +17,7 @@ function buildKeyboard() {
       key.classList.add('key');
       key.innerText = KEYBOARD_STRUCTURE[i][j].val[0];
       key.style['flex-grow'] = KEYBOARD_STRUCTURE[i][j].grow;
+      key.dataset.code = KEYBOARD_STRUCTURE[i][j].code;
       row.append(key);
     }
 
@@ -24,6 +25,38 @@ function buildKeyboard() {
   }
 }
 
+function buildTextArea() {
+  const textarea = document.createElement('textarea');
+  textarea.id = 'textarea';
+
+  document.querySelector('body').prepend(textarea);
+  document.getElementById('textarea').focus();
+}
+
+function putKeyboardEventHandlers() {
+  document.addEventListener('keydown', (event) => {
+    const code = event.code;
+    document.querySelector(`div[data-code="${code}"]`).classList.add('pressed');
+  }, false);
+
+  document.addEventListener('keyup', (event) => {
+    const code = event.code;
+    document.querySelector(`div[data-code="${code}"]`)
+        .classList.remove('pressed');
+  }, false);
+}
+
 document.addEventListener('DOMContentLoaded', ()=>{
   buildKeyboard();
+  buildTextArea();
+
+  const p1 = document.createElement('p');
+  p1.classList.add('notification');
+  p1.innerText = 'Клавиатура создана в операционной системе Windows/n/r';
+  const p2 = document.createElement('p');
+  p2.classList.add('notification');
+  p2.innerText = 'Для переключения языка комбинация: Win + Space';
+  document.querySelector('body').append(p1, p2);
+
+  putKeyboardEventHandlers();
 });
